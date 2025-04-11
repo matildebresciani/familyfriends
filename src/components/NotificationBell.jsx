de31143ccdb5ba55a55e115af2530e3bf013183a
matilde <mabr0005@stud.kea.dk>
2025-04-11 15:29:05 +0200

    noti bell zustand

--- src/components/NotificationBell.jsx
+++ src/components/NotificationBell.jsx
@@ -1,14 +1,21 @@
 "use client";
 import { FiBell } from "react-icons/fi";
+import { MdNotifications, MdNotificationsActive } from "react-icons/md";
 import useStore from "@/app/store/notificationStore";
 
 const NotificationBell = () => {
-    const { message } = useStore();
-    console.log("Bell: ", message)
-    return (     <div className="relative">
+    const { messages } = useStore();
+    console.log("Bell: ", messages)
+    return (     
+        // <div>
+        //     {messages > 0 ? <MdNotificationsActive /> : <MdNotifications />}
+        // </div>
+        <div className="relative">
             <FiBell size={24} className="text-black" />
-            <div className="absolute -top-1 right-0 w-3 h-3 bg-primary-color rounded-full border-2 border-white"></div> {/* Rød indikator */}
-          </div> );
+            {messages > 0 ? <div className="absolute -top-1 right-0 w-3 h-3 bg-primary-color rounded-full border-2 border-white"></div> : ""}
+        </div>
+          
+        );
 }
  
 export default NotificationBell;
